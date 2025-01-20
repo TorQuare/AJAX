79d717254db43c7e0f9795b4363cb4a7f72c3d7b
Patryk Bajgot <45692984+TorQuare@users.noreply.github.com>
2025-01-20 19:37:06 +0100

    Refactor

--- src/main/java/com/example/AJAX/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.AJAX.repository;
-
-import com.example.AJAX.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
